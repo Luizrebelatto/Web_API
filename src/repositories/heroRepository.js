0b26db41d844bb4d0788dee40b56e8a1a0787c49
Luiz Gabriel Rebelatto Bianchi <lbrebelatto@gmail.com>
2021-10-30 00:46:54 -0300

    feat: acess to data

--- src/repositories/heroRepository.js
+++ src/repositories/heroRepository.js
@@ -1,6 +1,19 @@
+const { readFile, writeFile } = require('fs/promisses')
+
 class HeroRepository {
     constructor({ file }) {
         this.file = file;
+    }
+
+    async _currentFileContent() {
+        return JSON.parse(await readFile(this.file));
+    }
+
+    async find(itemId){
+        const all = await this._currentFileContent();
+        if(!itemId) return all
+
+        return all.find(({ id }) => itemId === id );
     }
 }
 
